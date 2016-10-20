1510480dc7945aab51f71f02f822e6ee45787a52
kevin <kevinochieng548@gmail.com>
2016-10-20 12:00:37 +0300

    pull changes

--- lib/github_hook.rb
+++ lib/github_hook.rb
@@ -2,12 +2,20 @@ require 'sinatra/base'
 require 'time'
 
 class GithubHook < Sinatra::Base
+  set(:autopull) { production? }
+
  post '/update' do
    app.settings.reset!
    load app.settings.app_file
 
    content_type :txt
+   if settings.autopull?
+     #Pipe stder to stdout to make
+     #sure we display everything
+     `git pull 2>&1`
+   else
    "ok"
+   end
  end
 end
 
